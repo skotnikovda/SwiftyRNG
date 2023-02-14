d543e361131ce40a2aaf83a133cf30ca0da5e0c2
Daniil Skotnikov <dskotnikov@icloud.com>
2023-02-14 12:57:22 +0300

    feat: add normal, weibull, exponential and uniform distributions

--- Sources/SwiftyRNG/SwiftyRNG.swift
+++ Sources/SwiftyRNG/SwiftyRNG.swift
@@ -15,7 +15,19 @@ public class SwiftyRNG {
     public func gaussian(sigma: Double) -> Double {
         return gsl_ran_gaussian(sgsl_rng, sigma)
     }
+    public func normal(mu: Double, sigma: Double) -> Double {
+        return mu + gsl_ran_gaussian(sgsl_rng, sigma)
+    }
     public func rayleigh(sigma: Double) -> Double {
         return gsl_ran_rayleigh(sgsl_rng, sigma)
+    }
+    public func exponential(mu: Double) -> Double {
+        return gsl_ran_exponential(sgsl_rng, mu)
+    }
+    public func weibull(a: Double, b: Double) -> Double {
+        return gsl_ran_weibull(sgsl_rng, a, b)
+    }
+    public func uniform(a: Double, b: Double) -> Double {
+        return gsl_ran_flat(sgsl_rng, a, b)
     }
 }
